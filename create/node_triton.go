31be6d841a34aa6e311ad1e43a5c73b86d23b072
Chris Guevara <chrismguevara@gmail.com>
2018-01-19 11:23:54 -0800

    Fix copying of struct

--- create/node_triton.go
+++ create/node_triton.go
@@ -244,7 +244,7 @@ func newTritonNode(selectedClusterManager, selectedCluster string, remoteBackend
 
 	// Add new node to terraform config with the new hostnames
 	for _, newHostname := range newHostnames {
-		cfgCopy := *(&cfg)
+		cfgCopy := cfg
 		cfgCopy.Hostname = newHostname
 		err = state.Add(fmt.Sprintf(tritonNodeKeyFormat, newHostname), cfgCopy)
 		if err != nil {
